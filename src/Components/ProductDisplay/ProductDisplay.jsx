a384fa99f7b18d4178abf41c9b0f98ebba2f0ab7
R ARUNACHALAM <chandrikaarun1958@gmail.com>
2023-11-28 02:24:29 +0530

    "Some changes"

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
 import "./ProductDisplay.css";
-import star_icon from "../Assets/star_icon.png";
-import star_dull_icon from "../Assets/star_dull_icon.png";
+
+import React, { useContext } from "react";
+
 import { ShopContext } from "../../Context/ShopContext";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import star_icon from "../Assets/star_icon.png";
 
 const ProductDisplay = (props) => {
-
-  const {product} = props;
-  const {addToCart} = useContext(ShopContext);
+  const { product } = props;
+  const { addToCart } = useContext(ShopContext);
 
   return (
     <div className="productdisplay">
@@ -19,7 +20,11 @@ const ProductDisplay = (props) => {
           <img src={product.image} alt="img" />
         </div>
         <div className="productdisplay-img">
-          <img className="productdisplay-main-img" src={product.image} alt="img" />
+          <img
+            className="productdisplay-main-img"
+            src={product.image}
+            alt="img"
+          />
         </div>
       </div>
       <div className="productdisplay-right">
@@ -33,8 +38,12 @@ const ProductDisplay = (props) => {
           <p>(122)</p>
         </div>
         <div className="productdisplay-right-prices">
-          <div className="productdisplay-right-price-old">${product.old_price}</div>
-          <div className="productdisplay-right-price-new">${product.new_price}</div>
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
         </div>
         <div className="productdisplay-right-description">
           A lightweight, usually knitted, pullover shirt, close-fitting and with
@@ -44,16 +53,26 @@ const ProductDisplay = (props) => {
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
           <div className="productdisplay-right-sizes">
-            <div>S</div>
-            <div>M</div>
-            <div>L</div>
-            <div>XL</div>
-            <div>XXL</div>
+            <div>8</div>
+            <div>9</div>
+            <div>10</div>
+            <div>11</div>
+            <div>12</div>
           </div>
         </div>
-        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
-        <p className="productdisplay-right-category"><span>Category :</span> Women, T-shirt, Crop Top</p>
-        <p className="productdisplay-right-category"><span>Tags :</span> Modern, Latest</p>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
+        <p className="productdisplay-right-category">
+          <span>Category :</span> Women, T-shirt, Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags :</span> Modern, Latest
+        </p>
       </div>
     </div>
   );
